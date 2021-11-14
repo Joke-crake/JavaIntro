e7a815b813481eecfefabf3d787f64c5ade2ae30
Joke-crake <79933495+Joke-crake@users.noreply.github.com>
2021-11-14 20:13:04 +0300

    Update TextFileView.java

--- Modul 5/Task01/src/by/epam/jonline/task/TextFileView.java
+++ Modul 5/Task01/src/by/epam/jonline/task/TextFileView.java
@@ -1,7 +1,5 @@
 package by.epam.jonline.task;
 
-import org.w3c.dom.Text;
-
 public class TextFileView {
     public void printTextOnConsole(TextFile textFile){
         System.out.println(textFile.getText());
