58fc6427922b878b9842221b20e643a6e73611e2
Joke-crake <79933495+Joke-crake@users.noreply.github.com>
2021-11-14 20:08:38 +0300

    Delete Modul 5 directory

--- Modul 5/Task01/src/by/jonline/epam/task/TextFileView.java
+++
@@ -1,9 +0,0 @@
-package by.jonline.epam.task;
-
-import org.w3c.dom.Text;
-
-public class TextFileView {
-    public void printTextOnConsole(TextFile textFile){
-        System.out.println(textFile.getText());
-    }
-}
--- Modul 5/Task04/src/by/jonline/epam/task/storage/TreasureTxtStorage.java
+++
@@ -1,9 +0,0 @@
-package by.jonline.epam.task.storage;
-
-import by.jonline.epam.task.bean.Treasure;
-
-import java.util.List;
-
-public interface TreasureTxtStorage {
-    public void loadTreasures(List<Treasure> treasures);
-}
