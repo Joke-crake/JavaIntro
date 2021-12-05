0ceff64591b53cff9641afc8f9fdc85ed8aaf6cd
Joke-crake <79933495+Joke-crake@users.noreply.github.com>
2021-12-05 13:10:17 +0300

    Add files via upload

---
+++ Modul 6/Task01/src/by/epam/task/main/Main.java
@@ -0,0 +1,24 @@
+package by.epam.task.main;
+
+import by.epam.task.menuAction.MenuAction;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MenuAction menuAction = new MenuAction();
+        menuAction.start();
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
