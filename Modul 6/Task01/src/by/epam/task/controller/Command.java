0efaffa96121de4495f00402761fa6fbdf439817
Sergey <79933495+Joke-crake@users.noreply.github.com>
2021-12-18 19:38:33 +0300

    Delete Modul 6/Task01 directory

--- Modul 6/Task01/src/by/epam/task/controller/Command.java
+++
@@ -1,5 +0,0 @@
-package by.epam.task.controller;
-
-public interface Command {
-    String execute(String[] params);
-}
--- Modul 6/Task01/src/by/epam/task/controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package by.epam.task.controller;
-
-public interface Controller {
-
-    String doAction (String request);
-    //request "logination login="aa" password="pass"
-    //request "registration name="asd" surname="dfg"
-    //response code formatting string
-}
